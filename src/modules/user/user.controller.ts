d67acb6ec262c9f270e1274f48350fc66dadc2b4
navid bahrami <navidbahrami1996@gmail.com>
2025-02-17 22:29:00 +0330

    first route

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/:id')
+  findById(@Param('id' , ParseIntPipe) id : number){
+  }
+}
