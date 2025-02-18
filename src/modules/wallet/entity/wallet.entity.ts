1e9e2141e73a288d4871a23ee110921f19579118
navid bahrami <navidbahrami1996@gmail.com>
2025-02-18 17:52:24 +0330

    wallet entity

--- src/modules/wallet/entity/wallet.entity.ts
+++ src/modules/wallet/entity/wallet.entity.ts
@@ -6,12 +6,14 @@ import { UserEntity } from "src/modules/user/entity/user.entity";
 export class WalletEntity {
     @PrimaryGeneratedColumn('increment')
     id : number;
-    @CreateDateColumn()
-    created_at : Date;
     @Column({type : 'enum' , enum : WalletEnum})
     type : string; //type of our transaction , it can be deposit or withdraw.
+    @Column({type : 'numeric'})
+    amount : number;
     @Column()
     invoice_number : string;
+    @CreateDateColumn()
+    created_at : Date;
     @Column()
     userId : number;                                        //in many payment projects we should use setNull instead cascade because shouldnt delete users transactions in banking app.
     @ManyToOne(() => UserEntity , user => user.transactions , {onDelete : 'CASCADE'}) //many wallets can belong to one person(user).
