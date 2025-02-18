b5906aac86ebfa36ebc4f455f8dfaa4c0e730211
navid bahrami <navidbahrami1996@gmail.com>
2025-02-18 17:57:22 +0330

    add create user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
@@ -12,6 +13,15 @@ export class UserService {
     async findById(id : number){
         const user = await this.userRepository.findOneBy({id})
         if(!user) throw new NotFoundException()
+            return user;
+    }
+    async createUser(createDto : CreateUserDto){
+        const {mobile} = createDto
+        let user = await this.userRepository.findOneBy({mobile})
+        if(!user) {
+            user = this.userRepository.create(createDto);
+            return await this.userRepository.save(user)
+        }
             return user;
     }
 }
