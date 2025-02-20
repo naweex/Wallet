b12b79f85cefb2749581d56dd8a25f48620274ba
navid bahrami <navidbahrami1996@gmail.com>
2025-02-20 12:21:34 +0330

    products list

---
+++ src/modules/products.ts
@@ -0,0 +1,22 @@
+export const ProductList = [
+    {
+        id: 1,
+        name: "iPhone 13 Pro max",
+        price: 1500,
+    },
+    {
+        id: 2,
+        name: "notebook",
+        price: 200,
+    },
+    {
+        id: 3,
+        name: "coffee",
+        price: 150,
+    },
+    {
+        id: 4,
+        name: "keyboard",
+        price: 640,
+    },
+]
