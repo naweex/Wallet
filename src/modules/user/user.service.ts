1725dd568a78f93c8b09344cc0b08b4b6067c73e
navid bahrami <navidbahrami1996@gmail.com>
2025-02-20 12:21:14 +0330

    user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -7,21 +7,20 @@ import { CreateUserDto } from './user.dto';
 @Injectable()
 export class UserService {
     constructor(
-        @InjectRepository(UserEntity) private userRepository : Repository<UserEntity>
-    ){}
-
-    async findById(id : number){
-        const user = await this.userRepository.findOneBy({id})
-        if(!user) throw new NotFoundException()
-            return user;
+        @InjectRepository(UserEntity) private userRepository: Repository<UserEntity>
+    ) { }
+    async findById(id: number) {
+        const user = await this.userRepository.findOneBy({id});
+        if(!user) throw new NotFoundException();
+        return user;
     }
-    async createUser(createDto : CreateUserDto){
-        const {mobile} = createDto
-        let user = await this.userRepository.findOneBy({mobile})
+    async createUser(createDto: CreateUserDto) {
+        const {mobile} = createDto;
+        let user = await this.userRepository.findOneBy({mobile});
         if(!user) {
             user = this.userRepository.create(createDto);
             return await this.userRepository.save(user)
         }
-            return user;
+        return user;
     }
 }
