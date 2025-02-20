aa96ee0af0a650b214343ddb4a49dd10933944c3
navid bahrami <navidbahrami1996@gmail.com>
2025-02-20 12:20:33 +0330

    add new route

--- src/modules/wallet/wallet.controller.ts
+++ src/modules/wallet/wallet.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { WalletService } from './wallet.service';
-import { DepositDto } from './wallet.dto';
+import { DepositDto, WithdrawDto } from './wallet.dto';
 
 @Controller('wallet')
 export class WalletController {
@@ -9,6 +9,10 @@ export class WalletController {
   @Post('deposit')
   deposit(@Body() depositDto : DepositDto){
     return this.walletService.deposit(depositDto);
+  }
+  @Post('withdraw')
+  withdraw(@Body() withdrawDto: WithdrawDto){
+    return this.walletService.paymentByWallet(withdrawDto);
   }
 
 }
